25092e30d9740c832d31338d9193028dc9dd91bf
Aarush Chaudhary <lastsecret987@noice.aleeas.com>
2025-02-13 15:42:04 +0530

    Delete find_me.py

--- find_me.py
+++
@@ -1,8 +0,0 @@
-import pyttsx3 as tts
-engine=tts.init()
-engine.setProperty('rate', 150)
-engine.say("Follow sequence initiated")
-engine.runAndWait()
-print("Follow sequence initiated")
-with open("face_tracking_nopan_main_control.py") as f:
-    exec(f.read())
